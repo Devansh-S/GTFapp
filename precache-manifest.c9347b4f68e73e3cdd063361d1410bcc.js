aa7a687b1cdb7930511c1cc1ce39c86f2d8500c9
Devansh-S <devanshsingh596@gmail.com>
2020-04-26 23:54:02 +0530

    Updates

---
+++ precache-manifest.c9347b4f68e73e3cdd063361d1410bcc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "050167e0cdc7ab5fa06980684835752b",
+    "url": "/GTFapp/index.html"
+  },
+  {
+    "revision": "dc6f52d537b98a560114",
+    "url": "/GTFapp/static/css/main.eed33af9.chunk.css"
+  },
+  {
+    "revision": "c79aeb3e4c559fb448c0",
+    "url": "/GTFapp/static/js/2.df5884d2.chunk.js"
+  },
+  {
+    "revision": "78aa0676509153305bde072ed1e9cb1c",
+    "url": "/GTFapp/static/js/2.df5884d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc6f52d537b98a560114",
+    "url": "/GTFapp/static/js/main.33a7ee80.chunk.js"
+  },
+  {
+    "revision": "725a245507609e4e7e38",
+    "url": "/GTFapp/static/js/runtime-main.624bfac7.js"
+  },
+  {
+    "revision": "ba305fece77f417d3245838fc4ee690c",
+    "url": "/GTFapp/static/media/login.ba305fec.png"
+  }
+]);
